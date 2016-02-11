ae3790bc6f665fa98518a6ae7ddd39bc37b58be7
Mackenzie Morgan <mackenziem@mackenziem-SocialCode.local>
2016-02-11 16:50:27 -0500

    added summary

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,24 +46,65 @@ class EventsController < ApplicationController
     end
   end
 
-  def show
-    if params[:from] < params[:to]
-      from = DateTime.iso8601(params[:from]).to_s(:db)
-      to = DateTime.iso8601(params[:to]) + 1.seconds #make inclusive
+  def get_to_from(param1, param2)
+    if param1 < param2
+      from = DateTime.iso8601(param1).to_s(:db)
+      to = DateTime.iso8601(param2) + 1.seconds #make inclusive
       to = to.to_s(:db)
     else # catch user error where to/from are reversed
-      from = DateTime.iso8601(params[:to]).to_s(:db)
-      to = DateTime.iso8601(params[:from]) + 1.seconds
+      from = DateTime.iso8601(param2).to_s(:db)
+      to = DateTime.iso8601(param1) + 1.seconds
       to = to.to_s(:db)
     end
 
+    return from, to
+  end
+
+  def show
+    (from, to) = get_to_from(read_params[:from], read_params[:to])
     @events = Event.where(date: from..to).order(date: :asc)
+  end
+
+  def summary
+    eventlist = show
+
+    if read_params[:by] == "day"
+      dateformat = "%Y-%m-%d"
+    elsif read_params[:by] == "hour"
+      dateformat = "%Y-%m-%dT%H"
+    elsif read_params[:by] == "minute"
+      dateformat = "%Y-%m-%dT%H:%M"
+    else
+      render json: {status: :bad_request}, status: :bad_request
+    end
+
+    counts = {}
+    for event in eventlist
+      interval = event[:date].strftime(dateformat)
+      if ! counts[interval]
+        counts[interval] = {}
+        for type in Event.types
+          counts[interval][type[0]] = 0
+        end
+      end
+      counts[interval][event.type] += 1
+    end
+
+    @events = []
+    counts.each do |interval, counters|
+      counters['date'] = DateTime.parse(interval).strftime("%FT%TZ")
+      @events << counters
+    end
 
   end
 
   private
     def event_params
       params.permit(:event, :date, :user, :type, :message, :otheruser)
+    end
+
+    def read_params
+      params.permit(:event, :to, :from, :by)
     end
 
 end
